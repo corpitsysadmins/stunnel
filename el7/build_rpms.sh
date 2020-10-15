ed54ba0a229ad84033612faf0c41a4c55366eb85
Irving Leonard <irving.leonard@modmed.com>
2020-10-15 12:43:12 -0400

    Initial commit

---
+++ el7/build_rpms.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+yum -y install rpmdevtools yum-utils
+
+rpmdev-setuptree
+
+for spec_files in *.spec; do
+	
+	if [ -e "$spec_files" ]; then
+		mv *.spec rpmbuild/SPECS/
+	else
+		echo "No new specs files were found"
+	fi
+    
+    break
+done
+
+yum-builddep -y rpmbuild/SPECS/stunnel.spec
+spectool -g -R rpmbuild/SPECS/stunnel.spec
+rpmbuild -ba rpmbuild/SPECS/stunnel.spec
